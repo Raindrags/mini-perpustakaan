8b181a93e3e9a7473aaacc5b690fa4ad8e96f776
Raindrags <raindrags12@gmail.com>
2025-09-19 11:08:44 +0700

    final 2

--- app/api/absensi/route.ts
+++ app/api/absensi/route.ts
@@ -62,7 +62,7 @@ export async function POST(req: Request) {
   } catch (error) {
     if (error instanceof z.ZodError) {
       return NextResponse.json(
-        { message: "Validasi gagal", errors: error.errors },
+        { message: "Validasi gagal", errors: error.issues },
         { status: 400 }
       );
     }
