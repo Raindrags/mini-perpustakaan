d1d5c428a02381083ac622f2963499b4eae5a0a0
Raindrags <raindrags12@gmail.com>
2025-09-19 09:55:56 +0700

    zzz

---
+++ app/notfound.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold">404</h1>
+      <p className="mt-2">Halaman yang kamu cari tidak ditemukan 🚧</p>
+    </div>
+  );
+}
